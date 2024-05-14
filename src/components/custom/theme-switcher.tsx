95385cc39a9354c9d94aec9f826d3e67e14a3e41
THEALIFHAKER1 <alifhaker1@gmail.com>
2024-05-13 19:33:37 -0700

    add icons organizer and update icons

--- src/components/custom/theme-switcher.tsx
+++ src/components/custom/theme-switcher.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -11,6 +10,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Icons } from "../icons/icons";
 
 export function ThemeSwitcher() {
   const { setTheme } = useTheme();
@@ -23,8 +23,8 @@ export function ThemeSwitcher() {
           size="icon"
           className="focus-visible:ring-0 focus-visible:ring-offset-0"
         >
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Icons.sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Icons.moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
