7fd56c39f4f4c9d2b49e54bbaf162e7caa479da8
THEALIFHAKER1 <alifhaker1@gmail.com>
2024-05-13 19:38:40 -0700

    minor cleanup

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -9,28 +9,26 @@ import Link from "next/link";
 function HomePage() {
   return (
     <>
-      <main className="flex h-full items-center justify-center">
-        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-          <Icons.logo className="h-20 w-20" />
-          <TextDecryption
-            targetText={siteConfig.name}
-            className="text-4xl font-semibold sm:text-5xl md:text-6xl lg:text-7xl"
-          />
-          <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-            {siteConfig.description}
-          </p>
-          <div className="flex gap-2">
-            <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              className={cn(buttonVariants({ size: "default" }))}
-            >
-              Get Started
-            </Link>
-            <ThemeSwitcher />
-          </div>
+      <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center h-full justify-center">
+        <Icons.logo className="h-20 w-20" />
+        <TextDecryption
+          targetText={siteConfig.name}
+          className="text-4xl font-semibold sm:text-5xl md:text-6xl lg:text-7xl"
+        />
+        <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+          {siteConfig.description}
+        </p>
+        <div className="flex gap-2">
+          <Link
+            href={siteConfig.links.github}
+            target="_blank"
+            className={cn(buttonVariants({ size: "default" }))}
+          >
+            Get Started
+          </Link>
+          <ThemeSwitcher />
         </div>
-      </main>
+      </div>
     </>
   );
 }
