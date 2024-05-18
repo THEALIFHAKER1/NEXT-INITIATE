44bc1315ae7217d58ac72ec2038f71ae258c47fd
THEALIFHAKER1 <alifhaker1@gmail.com>
2024-05-18 18:35:25 +0800

    remove height define from loader

--- src/components/layout/loader.tsx
+++ src/components/layout/loader.tsx
@@ -11,17 +11,16 @@ type LoaderProps = {
 export default function Loader({ loadingText }: LoaderProps) {
   return (
     <m.div
-      initial={{ opacity: 0, y: 0 }}
+      initial={{ opacity: 0 }}
       animate={{
         opacity: 1,
         transition: { duration: 0.5, ease: "easeInOut" },
       }}
       exit={{
         opacity: 0,
-        y: "100dvh",
         transition: { duration: 0.5, ease: "easeInOut" },
       }}
-      className="relative flex min-h-screen w-full flex-col items-center justify-center font-sans focus:focus-visible:focus:outline-none"
+      className="relative flex h-full w-full flex-col items-center justify-center font-sans focus:focus-visible:focus:outline-none"
     >
       <span className="z-[1] animate-pulse font-sans text-8xl sm:text-[30vh]">
         <Icons.logo className="h-32 w-32" />
